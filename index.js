29725825dbf6263d4fc6217becc084bbaa70c82e
bryanjo28 <48087968+bryanjo28@users.noreply.github.com>
2024-05-20 16:08:16 +0700

    updated

--- index.js
+++ index.js
@@ -32,6 +32,18 @@ class Player {
 	draw() {
 		// c.fillStyle = 'red'
 		// c.fillRect(this.position.x, this.position.y, this.width, this.height)
+		c.save()
+		c.translate(
+			player.position.x + player.width / 2,
+			player.position.y + player.height / 2
+		)
+		c.rotate(this.rotation)
+
+		c.translate(
+			-player.position.x - player.width / 2,
+			-player.position.y - player.height / 2
+		)
+
 		c.drawImage(
 			this.image,
 			this.position.x,
@@ -39,8 +51,10 @@ class Player {
 			this.width,
 			this.height
 		)
+		c.restore()
+
 	}
-	
+
 	update() {
 		if (this.image) {
 			this.draw()
@@ -49,8 +63,32 @@ class Player {
 	}
 }
 
-const player = new Player()
+//projectile
+class Projectile {
+	constructor({ position, velocity }) {
+		this.position = position,
+			this.velocity = velocity
 
+		this.radius = 3
+	}
+	draw() {
+		c.beginPath()
+		c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+		c.fillStyle = 'red'
+		c.fill()
+		c.closePath()
+	}
+
+	update() {
+		this.draw()
+		this.position.x += this.velocity.x
+		this.position.y += this.velocity.y
+	}
+}
+
+//call
+const player = new Player()
+const projectiles = []
 const keys = {
 	a: {
 		pressed: false
@@ -70,13 +108,25 @@ function animate() {
 	c.fillRect(0, 0, canvas.width, canvas.height)
 	player.update()
 
+	projectiles.forEach((projectile, index) => {
+		if (projectile.position.y + projectile.radius <= 0) {
+			setTimeout(() => {			
+			projectiles.splice(index, 1)
+			}, 0)
+		} else {
+			projectile.update()
+		}
+	})
+
 	if (keys.a.pressed && player.position.x >= 0) {
 		player.velocity.x = -10
 		player.rotation = -0.15
 	} else if (keys.d.pressed && player.position.x + player.width <= canvas.width) {
 		player.velocity.x = 10
+		player.rotation = 0.15
 	} else {
 		player.velocity.x = 0
+		player.rotation = 0
 	}
 }
 
@@ -93,7 +143,20 @@ addEventListener('keydown', ({ key }) => {
 			keys.d.pressed = true
 			break
 		case ' ':
-			console.log('space');
+			console.log('space')
+			console.log(projectiles)
+			projectiles.push(
+				new Projectile({
+					position: {
+						x: player.position.x + player.width / 2,
+						y: player.position.y
+					},
+					velocity: {
+						x: 0,
+						y: -10
+					}
+				})
+			)
 			break
 	}
 })
